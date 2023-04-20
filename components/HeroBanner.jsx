ffe987c6cb52e806f464ec7383b125bb66d99c2d
Fabio <fabiosct@gmail.com>
2023-04-20 20:58:01 -0300

    git add.

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const HeroBanner = () => {
+  return (
+    <div className='hero-banner-container'>
+      <p className='beats-solo'>Pequena descrição</p>
+      <h3>Mid Text</h3>
+      <img src="" alt="short" className="hero-banner-image"/>
+    </div>
+  )
+}
+
+export default HeroBanner;
